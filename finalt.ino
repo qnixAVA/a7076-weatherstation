3022676e9025af4ff3be429c5b7b6bdeb8ba3d56
Marius <49654973+JMLF@users.noreply.github.com>
2024-06-06 19:00:22 +0200

    Requests class

--- finalt.ino
+++ finalt.ino
@@ -1,10 +1,3 @@
-/**
- * @file      WeatherStation.ino
- * @author    Your Name
- * @date      2024-06-02
- * @note      This example combines weather measurements and HTTP POST request to send data to a server.
- */
-
 #define TINY_GSM_RX_BUFFER 1024 // Set RX buffer to 1Kb
 #define TINY_GSM_DEBUG Serial
 
@@ -13,18 +6,9 @@
 #include <Arduino.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
-#include "utilities.h"
-#include <TinyGsmClient.h>
 
-#ifdef DUMP_AT_COMMANDS
-#include <StreamDebugger.h>
-StreamDebugger debugger(SerialAT, Serial);
-TinyGsm modem(debugger);
-#else
-TinyGsm modem(SerialAT);
-#endif
+#include "includes/requests.hpp"
 
-const char *server_url = "http://rn134ha.duckdns.org/api/webhook/weather_station_webhook";
 
 // Pin definitions
 #define BATTERY_PIN 35  // ADC pin for battery
@@ -75,127 +59,26 @@ void setup() {
 
     // Initialize the temperature sensor
     sensors.begin();
+    Serial.println(F("Sensor calibration & sensor begin done"));
 
-    // Initialize the modem
-    SerialAT.begin(115200, SERIAL_8N1, MODEM_RX_PIN, MODEM_TX_PIN);
-    pinMode(BOARD_POWERON_PIN, OUTPUT);
-    digitalWrite(BOARD_POWERON_PIN, HIGH);
-    pinMode(MODEM_RESET_PIN, OUTPUT);
-    digitalWrite(MODEM_RESET_PIN, !MODEM_RESET_LEVEL);
-    delay(100);
-    digitalWrite(MODEM_RESET_PIN, MODEM_RESET_LEVEL);
-    delay(2600);
-    digitalWrite(MODEM_RESET_PIN, !MODEM_RESET_LEVEL);
-    pinMode(BOARD_PWRKEY_PIN, OUTPUT);
-    digitalWrite(BOARD_PWRKEY_PIN, LOW);
-    delay(100);
-    digitalWrite(BOARD_PWRKEY_PIN, HIGH);
-    delay(100);
-    digitalWrite(BOARD_PWRKEY_PIN, LOW);
-
-    // Check modem connection
-    int retry = 0;
-    while (!modem.testAT(1000)) {
-        Serial.println(".");
-        if (retry++ > 10) {
-            digitalWrite(BOARD_PWRKEY_PIN, LOW);
-            delay(100);
-            digitalWrite(BOARD_PWRKEY_PIN, HIGH);
-            delay(1000);
-            digitalWrite(BOARD_PWRKEY_PIN, LOW);
-            retry = 0;
-        }
+    //Init modem
+    Requests modem;
+    if(!modem.checkSim()) {
+        Serial.println("Sim check failed");
+        return;
     }
-    Serial.println();
-
-    // Check SIM card
-    SimStatus sim = SIM_ERROR;
-    while (sim != SIM_READY) {
-        sim = modem.getSimStatus();
-        switch (sim) {
-        case SIM_READY:
-            Serial.println("SIM card online");
-            break;
-        case SIM_LOCKED:
-            Serial.println("The SIM card is locked. Please unlock the SIM card first.");
-            break;
-        default:
-            break;
-        }
-        delay(1000);
+    if(!modem.registerNetwork()) {
+        Serial.println("network registration failed");
+        return;
     }
-
-#ifndef TINY_GSM_MODEM_SIM7672
-    if (!modem.setNetworkMode(MODEM_NETWORK_AUTO)) {
-        Serial.println("Set network mode failed!");
-    }
-    String mode = modem.getNetworkModes();
-    Serial.print("Current network mode : ");
-    Serial.println(mode);
-#endif
-
-    int16_t sq;
-    Serial.print("Wait for the modem to register with the network.");
-    RegStatus status = REG_NO_RESULT;
-    while (status == REG_NO_RESULT || status == REG_SEARCHING || status == REG_UNREGISTERED) {
-        status = modem.getRegistrationStatus();
-        switch (status) {
-        case REG_UNREGISTERED:
-        case REG_SEARCHING:
-            sq = modem.getSignalQuality();
-            Serial.printf("[%lu] Signal Quality:%d", millis() / 1000, sq);
-            delay(1000);
-            break;
-        case REG_DENIED:
-            Serial.println("Network registration was rejected, please check if the APN is correct");
-            return;
-        case REG_OK_HOME:
-            Serial.println("Online registration successful");
-            break;
-        case REG_OK_ROAMING:
-            Serial.println("Network registration successful, currently in roaming mode");
-            break;
-        default:
-            Serial.printf("Registration Status:%d\n", status);
-            delay(1000);
-            break;
-        }
-    }
-    Serial.println();
-
-    Serial.printf("Registration Status:%d\n", status);
-    delay(1000);
-
-    String ueInfo;
-    if (modem.getSystemInformation(ueInfo)) {
-        Serial.print("Inquiring UE system information:");
-        Serial.println(ueInfo);
-    }
-
-    if (!modem.enableNetwork()) {
-        Serial.println("Enable network failed!");
-    }
-
-    delay(5000);
-
-    String ipAddress = modem.getLocalIP();
-    Serial.print("Network IP:"); Serial.println(ipAddress);
-
-    // Initialize HTTPS
-    modem.https_begin();
-
-    // Set GET URT
-    if (!modem.https_set_url(server_url)) {
-        Serial.println("Failed to set the URL. Please check the validity of the URL!");
+    if(!modem.enableNetwork()) {
+        Serial.println("network enabling failed");
         return;
     }
 
-    modem.https_add_header("Accept-Language", "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6");
-    modem.https_add_header("Accept-Encoding", "gzip, deflate, br");
-    modem.https_add_header("Content-Type", "application/json");
-    modem.https_set_accept_type("application/json");
-    modem.https_set_user_agent("TinyGSM/LilyGo-A76XX");
+    delay(1000);
 }
+
 
 void loop() {
     // Read temperature
@@ -219,30 +102,20 @@ void loop() {
     float windSpeed = weatherMeterKit.getWindSpeed();
     float totalRainfall = weatherMeterKit.getTotalRainfall();
 
-    // Prepare the POST body
-    String post_body = "{\"temperature\":" + String(temperature, 2) + 
-                        ", \"battery_voltage\":" + String(batteryVoltage / 1000.0, 2) + 
-                        ", \"solar_voltage\":" + String(solarVoltage / 1000.0, 2) + 
-                        ", \"wind_direction\":" + String(windDirection, 2) + 
-                        ", \"wind_speed\":" + String(windSpeed, 2) + 
-                        ", \"total_rainfall\":" + String(totalRainfall, 2) + "}";
-
-    // Send HTTP POST request
-    int httpCode = modem.https_post(post_body);
-    if (httpCode != 200) {
-        Serial.print("HTTP POST request failed! Error code = ");
-        Serial.println(httpCode);
+    if(!modem.initHttps()) {
+        Serial.println("HTTPS init failed");
         return;
     }
 
-    // Read and print the HTTP response
-    String header = modem.https_header();
-    Serial.print("HTTP header:");
-    Serial.println(header);
+    // Prepare the POST body
+    String post_body = "{\"temperature\":" + String(temperature, 2) +
+                        ", \"battery_voltage\":" + String(batteryVoltage / 1000.0, 2) +
+                        ", \"solar_voltage\":" + String(solarVoltage / 1000.0, 2) +
+                        ", \"wind_direction\":" + String(windDirection, 2) +
+                        ", \"wind_speed\":" + String(windSpeed, 2) +
+                        ", \"total_rainfall\":" + String(totalRainfall, 2) + "}";
 
-    String body = modem.https_body();
-    Serial.print("HTTP body:");
-    Serial.println(body);
+    modem.postHttpsRequests(post_body);
 
     delay(60000); // Delay for 1 minute before next measurement
 }
