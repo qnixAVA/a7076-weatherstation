efb014c977656133ff5b06ff23e6e1f0c9fe9d5b
Marius <49654973+JMLF@users.noreply.github.com>
2024-06-08 14:35:25 +0200

    Deepsleep integration

---
+++ script/utilities.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#define LILYGO_T_A7670
+
+#if defined(LILYGO_T_A7670)
+
+#define MODEM_BAUDRATE                      (115200)
+#define MODEM_DTR_PIN                       (25)
+#define MODEM_TX_PIN                        (26)
+#define MODEM_RX_PIN                        (27)
+// The modem boot pin needs to follow the startup sequence.
+#define BOARD_PWRKEY_PIN                    (4)
+#define BOARD_ADC_PIN                       (35)
+// The modem power switch must be set to HIGH for the modem to supply power.
+#define BOARD_POWERON_PIN                   (12)
+#define MODEM_RING_PIN                      (33)
+#define MODEM_RESET_PIN                     (5)
+#define BOARD_MISO_PIN                      (2)
+#define BOARD_MOSI_PIN                      (15)
+#define BOARD_SCK_PIN                       (14)
+#define BOARD_SD_CS_PIN                     (13)
+#define BOARD_BAT_ADC_PIN                   (35)
+#define MODEM_RESET_LEVEL                   HIGH
+#define SerialAT                            Serial1
+
+#define MODEM_GPS_ENABLE_GPIO               (-1)
+
+
+#ifndef TINY_GSM_MODEM_A7670
+#define TINY_GSM_MODEM_A7670
+#endif
+
+#else
+#error "Use ArduinoIDE, please open the macro definition corresponding to the board above <utilities.h>"
+#endif
